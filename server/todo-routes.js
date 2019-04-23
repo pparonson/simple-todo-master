f9c6a174f1e33147adbfcde17cb842b6f1176a13
pparonson <preston.aronson@gmail.com>
2019-04-23 14:51:16 -0600

    add db implementation status comments

--- server/todo-routes.js
+++ server/todo-routes.js
@@ -6,7 +6,7 @@ const todoExpress = express();
 //todoExpress.get('/todo', (req, res) => TodoData.findAll().then(todos => res.json(todos)).catch((err) => res.status(500).json(err)));
 todoExpress.get('/todo', TodosController.findAll)
 //todoExpress.post('/todo', (req, res) => TodoData.create(req.body).then(todo => res.json(todo)).catch((err) => res.status(500).json(err)));
-todoExpress.post('/todo', TodosController.create)
+todoExpress.post('/todo', TodosController.create) // TODO: NOT WORKING IN FRONTEND
 // todoExpress.delete('/todo/:id', (req, res) => TodoData.delete(req.params.id).then(() => res.sendStatus(200)).catch((err) => res.status(500).json(err)));
 // todoExpress.put("/todo/:id", (req, res) => {
 //   TodoData.update(req.params.id, req.body)
@@ -15,6 +15,6 @@ todoExpress.post('/todo', TodosController.create)
 //     })
 //     .catch(err => res.status(500).json(err));
 // });
-todoExpress.put("/todo/:id", TodosController.update)
+todoExpress.put("/todo/:id", TodosController.update) // TODO: NOT WORKING
 
 export default todoExpress;
